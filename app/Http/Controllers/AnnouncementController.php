fcc7c13a9e9747ccdf2fc8819bd137b45db1bf5f
Nicola Milella <nicombk@gmail.com>
2021-08-07 18:54:20 +0200

    basi announcement api crud

---
+++ app/Http/Controllers/AnnouncementController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AnnouncementCollection;
+use App\Http\Resources\AnnouncementResource;
+use App\Models\Announcement;
+use Illuminate\Http\Request;
+
+class AnnouncementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new AnnouncementCollection(Announcement::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return new AnnouncementResource(Announcement::create($request->all()));
+        // return response()->json('created',201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Announcement $announcement)
+    {
+        return new AnnouncementResource($announcement);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Announcement $announcement)
+    {
+        if($announcement->update($request->all()))
+            return new AnnouncementResource($announcement);
+        
+        return response()->json(['message' => 'Error'], 400);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Announcement $announcement)
+    {
+        if($announcement->delete())
+            return response()->json('success',200);
+    
+    }
+}
